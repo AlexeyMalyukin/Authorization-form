0c4685098a0ac87174080a1073fd9d31c4c9e619
Alexey Malyukin <alexeymalyukin@gmail.com>
2022-06-18 21:57:57 +0300

    Validation of form is added
    Validation of every form is added. 4 symbols at least for login and password, special filter for email.

--- app.js
+++ app.js
@@ -39,30 +39,46 @@ function User(login, password, email) {
     this.email = email;
 }
 
-const saveData = () => {
-    login.value != '' && password.value != '' && email.value != '' ? 
-    (localStorage.setItem('data', JSON.stringify(data)), 
-    success.style.display = 'flex',
-    inputLogin.style.display = 'none',
-    inputAuthor.style.display = 'none',
-    successUser.innerHTML = `Congratulations, ${login.value}!
-    <button class="option have_acc" onclick="haveAccount()">Log in now</button>`)
-    
-    : notification.innerHTML = 'enter data!' ;  
-    
-}
+const loginNote = document.querySelector('.login_note');
+const passwordNote = document.querySelector('.password_note');
+const emailNote = document.querySelector('.email_note');
+//const alertLogin = document.createElement('p');
 
+const emailReqepx = new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g);
+const basicReqepx = new RegExp(/^\S{4,}$/)
 
 submit.addEventListener('click', () => {
-    data.push(new User(login.value, password.value, email.value));
-    saveData();
-   
-    //showUsers();
-    //haveAccount();
-    //login.value = '';
-    //password.value = '';
-    //email.value = '';
-});
+    if (basicReqepx.test(login.value) && basicReqepx.test(password.value) && emailReqepx.test(email.value)) {
+        data.push(new User(login.value, password.value, email.value)); 
+        localStorage.setItem('data', JSON.stringify(data)); 
+        success.style.display = 'flex';
+        inputLogin.style.display = 'none';
+        inputAuthor.style.display = 'none';
+        successUser.innerHTML = `Congratulations, ${login.value}!
+        <button class="option have_acc" onclick="haveAccount()">Log in now</button>`;
+    } else {
+        if(basicReqepx.test(login.value)) {
+            loginNote.innerHTML = "";
+        } else {
+            loginNote.innerHTML = 'Your login should constsit at least 4 symbols'; 
+            
+        }
+
+        if(basicReqepx.test(password.value)) {
+            passwordNote.innerHTML = "";
+        } else {
+            passwordNote.innerHTML = 'Your password should constsit at least 4 symbols';   
+        }
+
+        
+        if(emailReqepx.test(email.value)) {
+            emailNote.innerHTML = "";
+        } else {
+                emailNote.innerHTML = "Email is not valid, try format: example@mail.com"; 
+        }
+    }
+})
+
 
 const showUsers = () => {
     users.innerHTML = "";
@@ -85,12 +101,12 @@ const checkIn = () => {
             (welcome.style.display = 'block', 
             welcomeUser.innerHTML = `Welcome back, ${user.log}!`, 
             inputLogin.style.display = 'none', 
-            inputAuthor.style.display = 'none'/*, user.idex--*/)
+            inputAuthor.style.display = 'none')
             : data.indexOf(user) +1
         }, result.innerHTML = 'incorrect data!', 
         welcome.style.display = 'none')
     
-    : result.innerHTML = 'enter data!';
+    : result.innerHTML = 'Your login or password is incorrect, please try again!';
       
 };
 
@@ -105,12 +121,15 @@ submitAuthor.addEventListener('click', () => {
 const createAccount = () => {
     inputLogin.style.display = 'block';
     inputAuthor.style.display = 'none';
+    
 
 }
 
 createAcc.addEventListener('click', () => {
     createAccount();
-    password.value = '';
+    notification.innerHTML = '';
+    result.innerHTML = '';
+    
 });
 
 
@@ -118,8 +137,11 @@ const haveAccount = () => {
     inputLogin.style.display = 'none';
     inputAuthor.style.display = 'block';
     success.style.display = 'none';
+    
 }
 
 haveAcc.addEventListener('click', () => {
     haveAccount();
+    notification.innerHTML = '';
+    result.innerHTML = '';
 })
